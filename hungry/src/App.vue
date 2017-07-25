308cbc51c140cd13a558ce734034f156414c211f
cai <980356828@qq.com>
2017-07-25 14:55:31 +0800

    route页面

--- hungry/src/App.vue
+++ hungry/src/App.vue
@@ -1,11 +1,38 @@
 <template>
   <div id="app">
-    dsdsds
+      <v-header></v-header>
+      <div class="tab">
+        <div class="tab">
+           <div class="tab-item">商品</div>
+           <div class="tab-item">评论</div>
+            <div class="tab-item">商家</div>
+        </div>
+      </div>
+      <div class="content">
+        content
+      </div>
   </div>
 </template>
 
 <script>
+import header from './components/header/header'
+export default{
+  components:{
+    'v-header':header
+  }
+}
 </script>
+<style lang="stylus">
+#app
+  .tab
+    display:flex
+    width :100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+         flex :4
+         text-align center
 
-<style>
+
+    
 </style>
