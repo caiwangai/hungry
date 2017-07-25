d5e20dd417637973b37fcf5154fe1f5dd8a367b8
cai <980356828@qq.com>
2017-07-25 18:11:07 +0800

    路由拦截alias

--- hungry/src/router/index.js
+++ hungry/src/router/index.js
@@ -1,27 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import goods from '../components/goods/goods'
-import seller from '../components/seller/seller.vue'
-import ratings from '../components/ratings/ratings.vue'
+import goods from 'components/goods/goods'
+import seller from 'components/seller/seller.vue'
+import ratings from 'components/ratings/ratings.vue'
 Vue.use(Router)
-
 export default new Router({
+    base: '/ratings',
     routes: [
         // {
         //   path: '/',
         //   name: 'Hello',
         //   component: Hello
         // },
+
         {
             path: "/goods",
             component: goods
         }, {
             path: "/seller",
-            component: seller
+            component: seller,
+            alias: '*'
         },
         {
             path: "/ratings",
             component: ratings
         }
+        // },
+        // next("/ratings")
     ]
 })
